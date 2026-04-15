c34a292959ae1ed702792fe9161767c4579e851c
Samar Ahmad <codewithsamsol@gmail.com>
2026-04-16 00:04:20 +0530

    Add files via upload

---
+++ src/views/meetups/HyderabadMeetups.vue
@@ -0,0 +1,18 @@
+<template>
+  <RegionMeetups :config="config" />
+</template>
+
+<script>
+import RegionMeetups from '@/components/RegionMeetups.vue'
+import { regionConfigs } from './regionConfigs'
+
+export default {
+  name: 'HyderabadMeetups',
+  components: { RegionMeetups },
+  data() {
+    return {
+      config: regionConfigs.hyderabad,
+    }
+  },
+}
+</script>
